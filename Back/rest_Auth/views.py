d8b23cf3790bf1f24004c4f0732d2f95cc89281d
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-07-09 00:03:00 -0400

    Serializers de Auth

--- Back/rest_Auth/views.py
+++ Back/rest_Auth/views.py
@@ -1,3 +1,46 @@
+from dataclasses import dataclass
+from msilib.schema import ServiceInstall
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
+
 
 # Create your views here.
+@api_view(['POST'])
+def singUp(request):
+    data = JSONParser().parse(request)
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CRATED)
+    else:
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login(request):
+    try:
+        data = JSONParser().parse(request)
+    
+        username = data['username']
+        password = data['password']
+    except:
+        return Response("Campos del Body deben ser username y password")
+    
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response("Usuario no se encuentra en los registros")
+    
+    pass_valido = check_password(password, user.password)
+    if not pass_valido:
+        return Response("Contraseña incorrecta, intente nuevamente.")
+    token, create = Token.objects.get_or_create(user=user)
+    return Response(token.key)
