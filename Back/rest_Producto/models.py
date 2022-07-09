29e59c300f34a6f513f2b3825badc41939264e94
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-07-09 12:49:26 -0400

    Entidad Categoria

--- Back/rest_Producto/models.py
+++ Back/rest_Producto/models.py
@@ -1,7 +1,13 @@
+from contextlib import nullcontext
+from tabnanny import verbose
 from django.db import models
 
 
 # Create your models here.
+class Categoria(models.Model):
+    idCategoria = models.AutoField(max_length=255, null=False, verbose_name="id categoria")
+    nombreCategoria = models.CharField(max_length=255, null=False, verbose_name="nombre categoria")
+
 class Producto(models.Model):
     idProducto = models.AutoField(primary_key=True, verbose_name="id de producto")
     nombre = models.CharField(max_length=255, null=False, verbose_name="Nombre de producto")
@@ -9,6 +15,7 @@ class Producto(models.Model):
     descripcion = models.CharField(max_length=255,null=False, verbose_name="Descripcion de producto")
     stock = models.IntegerField(null=False, default=0, verbose_name="Stock de producto")
     imagen = models.CharField(max_length=512, null=False, verbose_name="Imagen de producto")
+    categoria = models.ForeignKey(Categoria, null=False, verbose_name="Categoria de producto")
 
 class Promocion(models.Model):
     idPromocion = models.AutoField(primary_key=True, verbose_name="id promocion")
