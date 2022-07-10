a6a1d9adc5ddb41b6a59f8d1d6a74dc62f1bf756
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-07-09 22:48:09 -0400

    url auth

--- Back/rest_Auth/urls.py
+++ Back/rest_Auth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from rest_Auth.views import singUp
+from rest_Auth.views import singUp, login
 
 urlpatterns = [
     path('singUp', singUp, name='singUp'),
+    path('login', login, name='login'),
 ]
