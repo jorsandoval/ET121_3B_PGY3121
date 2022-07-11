fa4cb7a695a5cdadf8ccffdb7264f7f02941537c
InSight1910 <cvveglia@hotmail.com>
2022-07-11 01:07:54 -0400

    se agrega logica de login y signup

--- Back/rest_Auth/urls.py
+++ Back/rest_Auth/urls.py
@@ -3,9 +3,9 @@ from rest_Auth.views import singUp, login, changePassword
 from .views import usuariosGetAll, usuarioById
 
 urlpatterns = [
-    path("singUp", singUp, name="singUp"),
+    path("signUp", singUp, name="singUp"),
     path("login", login, name="login"),
     path("changePassword", changePassword, name="changePassword"),
-    path('usuarios/', usuariosGetAll, name='usuariosGetAll'),
-    path('usuarios/<idUsuario>', usuarioById, name='usuarioById'),
+    path("usuarios/", usuariosGetAll, name="usuariosGetAll"),
+    path("usuarios/<idUsuario>", usuarioById, name="usuarioById"),
 ]
