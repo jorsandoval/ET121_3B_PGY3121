86d9a4920297a3ee6690ed44100acc260a8201ad
InSight1910 <cvveglia@hotmail.com>
2022-07-12 00:41:48 -0400

    Se añade guardado de productos a localstorage

--- Front/script/verProducto.js
+++ Front/script/verProducto.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", async (e) => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const idProducto = (urlParams.get('id'));
-    const response = await (
-    await fetch(`http://localhost:8000/api/v1/Productos/productos/${idProducto}`)
+	const urlParams = new URLSearchParams(window.location.search);
+	const idProducto = urlParams.get("id");
+	const response = await (
+		await fetch(
+			`http://localhost:8000/api/v1/Productos/productos/${idProducto}`
+		)
 	).json();
-    const container = document.getElementById("detalleProduto");
-    container.innerHTML = `<div class="row"><div class="d-flex justify-content-center col-lg-6"><img src="../${response.imagen}"alt=" "class="img-fluid"/></div><div class="col-lg-6 d-flex"style="flex-direction: column; justify-content: space-evenly"><h1 class="page-header">${response.nombre}</h1><ul class="list-inline" style="list-style: none"><li><p>$${response.valor}</p></li><li><p>Stock: ${response.stock} Und.</p></li><li><p>${response.descripcion}</p></li></ul><button class="btn btn-block"style="background-color: rgb(133, 185, 11); color: #fff"type="submit">Agregar carrito</button></div></div>`
-})
+	const container = document.getElementById("detalleProduto");
+	container.innerHTML = `<div class="row"><div class="d-flex justify-content-center col-lg-6"><img src="../${response.imagen}"alt=" "class="img-fluid"/></div><div class="col-lg-6 d-flex"style="flex-direction: column; justify-content: space-evenly"><h1 class="page-header">${response.nombre}</h1><ul class="list-inline" style="list-style: none"><li><p>$${response.valor}</p></li><li><p>Stock: ${response.stock} Und.</p></li><li><p>${response.descripcion}</p></li></ul><button class="btn btn-block"style="background-color: rgb(133, 185, 11); color: #fff"type="button" id="button">Agregar carrito</button></div></div>`;
 
-
+	document.getElementById("button").addEventListener("click", (e) => {
+		if (localStorage.getItem("carrito") == null) {
+			localStorage.setItem(
+				"carrito",
+				JSON.stringify([new URLSearchParams(window.location.search).get("id")])
+			);
+		} else {
+			const localDB = JSON.parse(localStorage.getItem("carrito"));
+			localDB.push(new URLSearchParams(window.location.search).get("id"));
+			localStorage.setItem("carrito", JSON.stringify(localDB));
+		}
+	});
+});
